a37a3a46d5844004ee9dcab28f5248c6072999b0
JoyRume <joy.rume@student.moringaschool.com>
2024-01-16 21:29:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 // Write your solution here!
-const cats=["Milo", "Otis", "Garfield"]
-
+const cats = ["Milo" , "Otis" , "Garfield"]
 function destructivelyAppendCat(name){
-   cats.push(name)
+    cats.push('Ralph');
+    return (cats);
 }
-
 function destructivelyPrependCat(name){
-    cats.unshift(name)
+    cats.unshift('Bob');
 }
-
-function destructivelyRemoveLastCat(){
-    cats.pop()
-}
-
+function destructivelyRemoveLastCat(name){
+    cats.pop('Garfield');
+    return (cats);
+}    
 function destructivelyRemoveFirstCat(){
-    cats.shift()
+    cats.shift('Milo')
 }
 
 function appendCat(name){
-    return cats.concat(name);
+    return [... cats, name];
 }
-
 function prependCat(name){
- return [name, ...cats]
+    return [name,...cats];
 }
-
 function removeLastCat(){
-   return cats.slice(0, -1)
+    return cats.slice(0, cats.length -1)
 }
-
 function removeFirstCat(){
-  return  cats.slice(1)
+    return cats.slice(1)
 }
